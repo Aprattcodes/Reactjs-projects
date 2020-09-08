3c49a77af911bbdd7c70c0b019fbbbe2c9873dbc
Alyssa Pratt <alyssakpratt@gmail.com>
2020-09-08 12:26:46 -0500

    adds product page styling and image links file

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,10 +1,12 @@
+
+
 export default {
     products: [
         {
         _id: '1',
         name: 'Godzilla',
         category: 'Toys',
-        image: '/image/toy.jpg',
+        image: "https://i.ibb.co/QbWqJTS/toy.jpg",
         price: 25,
         brand: 'Tokyo',
         rating: 4.5,
@@ -14,7 +16,7 @@ export default {
         _id: '2',
         name: 'Army Men',
         category: 'Toys',
-        image: '/image/army.jpg',
+        image: "https://i.ibb.co/25Hs07P/army.jpg",
         price: 5,
         brand: 'Playco',
         rating: 4,
@@ -24,7 +26,7 @@ export default {
         _id: '3',
         name: 'Barrel of Monkies',
         category: 'Toys',
-        image: '/image/barrel-of-monkies.jpg',
+        image: "https://i.ibb.co/QcRwfH4/barrel-of-monkeys.jpg",
         price: 10,
         brand: 'Nabisco',
         rating: 3,
@@ -34,7 +36,7 @@ export default {
         _id: '4',
         name: 'Furby',
         category: 'Toys',
-        image: '/image/furby.jpg',
+        image: "https://i.ibb.co/Fx4X2tt/furby.jpg",
         price: 25,
         brand: 'Tokyo',
         rating: 5,
@@ -44,7 +46,7 @@ export default {
         _id: '5',
         name: 'Rubik',
         category: 'Toys',
-        image: '/image/rubik.jpg',
+        image: "https://i.ibb.co/yYkdVrr/rubik.jpg",
         price: 10,
         brand: 'Playco',
         rating: 4.5,
