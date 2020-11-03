36064cc44d90703f4bb7e9c27822e14a9c7603d0
Alyssa Pratt <alyssakpratt@gmail.com>
2020-11-03 08:36:51 -0600

    start frontend react app

--- ecommerce/shop-me/frontend/src/App.js
+++ ecommerce/shop-me/frontend/src/App.js
@@ -1,62 +1,101 @@
-import React from 'react';
-import data from './data';
-import {BrowserRouter, Route, Link } from 'react-router-dom';
+import logo from './logo.svg';
 import './App.css';
-import HomeScreen from './Screens/HomeScreen';
-import ProductScreen from './Screens/ProductScreen';
-
 
 function App() {
-  
-  const openMenu = () => {
-    document.querySelector(".sidebar").classList.add("open");
-  }
-  const closeMenu = () => {
-    document.querySelector(".sidebar").classList.remove("open");
-  }
-
   return (
-    <BrowserRouter>
-    <div className="grid-container">
-        <header className="header">
-            <div className="brand">
-                <button onClick={openMenu}>
-                    &#9776;
-                </button>
-            <Link to='/'>Shopme</Link>
-            </div>
-            <div className="header-links">
-                <a href="cart.html">Cart</a>
-                <a href="signin.html">Sign in</a>
-            </div>
-        </header>
-        <aside className="sidebar">
-            <h3>Shopping Categories</h3>
-            <button className="sidebar-close-button" onClick={closeMenu}>X</button>
-            <ul>
-                <li>
-                    <a href="index.html">American classics</a>
-                </li>
-                <li>
-                    <a href="index.html">Japanese</a>
-                </li>
-                <li>
-                    <a href="index.html">Random</a>
-                </li>
-            </ul>
-        </aside>
-        <main className="main">
-            <div className="content">
-                <Route path="/product/:id" component={ProductScreen} />
-                <Route path="/" exact={true} component={HomeScreen} />
-            
-            </div>
-        </main>
-        <footer className="footer">
-            All rights reserved
-        </footer>
-    </div>
-    </BrowserRouter>
+<div className="grid-container">
+    <header className="header">
+        <div className="brand">
+            <button onclick="openMenu()">
+                &#9776;
+            </button>
+        <a href="index.html">Shopme</a>
+        </div>
+        <div className="header-links">
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign in</a>
+        </div>
+    </header>
+    <aside className="sidebar">
+        <h3>Shopping Categories</h3>
+        <button className="sidebar-close-button" onclick="closeMenu()">X</button>
+        <ul>
+            <li>
+                <a href="index.html">American Classics</a>
+            </li>
+            <li>
+                <a href="index.html">Japanese</a>
+            </li>
+            <li>
+                <a href="index.html">Random</a>
+            </li>
+        </ul>
+    </aside>
+    <main className="main">
+        <div className="content">
+        <ul className="products">
+            <li>
+                <div className="product">
+                    <img className="product-image" src="images/toy.jpg" alt="godzilla"/>
+                    <div className="product-name">
+                        <a href="product.html">Godzilla</a>
+                    </div>
+                    <div className="product-brand">Tokyo</div>
+                    <div className="product-price">$25</div>
+                    <div className="product-rating">4.5 stars (10 reviews)</div>
+                </div>
+            </li>
+            <li>
+                <div className="product">
+                    <img className="product-image" src="C:\Users\alyss\Pictures\toy.jpg" alt="godzilla"/>
+                    <div className="product-name">
+                        <a href="product.html">Godzilla</a>
+                    </div>
+                    <div className="product-brand">Tokyo</div>
+                    <div className="product-price">$25</div>
+                    <div className="product-rating">4.5 stars (10 reviews)</div>
+                </div>
+            </li>
+            <li>
+                <div className="product">
+                    <img className="product-image" src="C:\Users\alyss\Pictures\toy.jpg" alt="godzilla"/>
+                    <div className="product-name">
+                        <a href="product.html">Godzilla</a>
+                    </div>
+                    <div className="product-brand">Tokyo</div>
+                    <div className="product-price">$25</div>
+                    <div className="product-rating">4.5 stars (10 reviews)</div>
+                </div>
+            </li>
+            <li>
+                <div className="product">
+                    <img className="product-image" src="C:\Users\alyss\Pictures\toy.jpg" alt="godzilla"/>
+                    <div className="product-name">
+                        <a href="product.html">Godzilla</a>
+                    </div>
+                    <div className="product-brand">Tokyo</div>
+                    <div className="product-price">$25</div>
+                    <div className="product-rating">4.5 stars (10 reviews)</div>
+                </div>
+            </li>
+            <li>
+                <div className="product">
+                    <img className="product-image" src="C:\Users\alyss\Pictures\toy.jpg" alt="godzilla"/>
+                    <div className="product-name">
+                        <a href="product.html">Godzilla</a>
+                      </div>
+                    <div className="product-brand">Tokyo</div>
+                    <div className="product-price">$25</div>
+                    <div className="product-rating">4.5 stars (10 reviews)</div>
+                </div>
+            </li>         
+        </ul>
+        </div>
+    </main>
+    <footer className="footer">
+        All rights reserved
+    </footer>
+</div>
   );
 }
 
