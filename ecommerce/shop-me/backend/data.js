06d49d01b4f7642f3eb7c9c615e27684bc62b35b
Alyssa Pratt <alyssakpratt@gmail.com>
2020-11-03 08:19:08 -0600

    initial commit for shop-me

---
+++ ecommerce/shop-me/backend/data.js
@@ -0,0 +1,56 @@
+
+
+export default {
+    products: [
+        {
+        _id: '1',
+        name: 'Godzilla',
+        category: 'Toys',
+        image: "toy.jpg",
+        price: 25,
+        brand: 'Tokyo',
+        rating: 4.5,
+        numReviews: 10,
+    },
+    {
+        _id: '2',
+        name: 'Army Men',
+        category: 'Toys',
+        image: "army.jpg",
+        price: 5,
+        brand: 'Playco',
+        rating: 4,
+        numReviews: 15,
+    },
+    {
+        _id: '3',
+        name: 'Barrel of Monkies',
+        category: 'Toys',
+        image: "barrel-of-monkeys.jpg",
+        price: 10,
+        brand: 'Nabisco',
+        rating: 3,
+        numReviews: 5,
+    },
+    {
+        _id: '4',
+        name: 'Furby',
+        category: 'Toys',
+        image: "furby.jpg",
+        price: 25,
+        brand: 'Tokyo',
+        rating: 5,
+        numReviews: 10,
+    },
+    {
+        _id: '5',
+        name: 'Rubik',
+        category: 'Toys',
+        image: "rubik.jpg",
+        price: 10,
+        brand: 'Playco',
+        rating: 4.5,
+        numReviews: 10,
+    },
+]
+}
